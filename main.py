b292170ccc07de3113c3376fc5a32beb2332fbd5
EeErNu <eerik.nurme@gmail.com>
2019-08-14 23:18:32 +0300

    Email sending functionality added

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 import numpy as np
 import cv2
+import smtplib
+import send_email
 
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+def faceDetection():
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Find faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
+        # Find faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        # Checks if face is detected; if so - take a picture
-        if (x, y, w , h) != None:
-            camera_port = 0
-            ramp_frames = 1
+            # Checks if face is detected; if so - take a picture
+            if (x, y, w , h) != None:
+                ramp_frames = 1
 
-            def get_image():
-                retval, im = cap.read()
-                return im
+                def get_image():
+                    retval, im = cap.read()
+                    return im
 
-            for i in xrange(ramp_frames):
-                temp = get_image()
+                for i in xrange(ramp_frames):
+                    get_image()
 
-            print("Taking image...")
-            camera_capture = get_image()
-            file = "test_image.png"
-            cv2.imwrite(file, camera_capture)
+                camera_capture = get_image()
+                file = "test_image.png"
+                cv2.imwrite(file, camera_capture)
+                # send_email.sendEmail(file)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__== "__main__":
+    faceDetection()
+
